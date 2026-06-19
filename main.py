745aa345353eda72e2309496fad09d7f9cb7ac1f
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-06-19 20:34:09 +0200

    added the project Structure with folders needed and files needed

---
+++ main.py
@@ -0,0 +1,28 @@
+
+
+
+
+# Be mindful of off quantity, price e.g Negative price or quantinty
+# Same transaction id transactions
+# Product can be none And I have to account that
+# Category Can be None but since if we have product we can actually take all products we have and category and autofill that with similar category same as product
+# Missing Values
+
+
+# Off data types for every category , check if the datatype is a valid datatype for every coloumn 
+
+# Off pricing 
+
+
+# Detect and report
+    # Example
+# Records Read: 10
+# Valid Records: 7
+# Invalid Records: 3
+        # or
+# invalid_records
+# Store:
+# transaction_id	reason
+# 8	Negative Quantity
+# 9	Negative Price
+# 15	Invalid Quantity Type
