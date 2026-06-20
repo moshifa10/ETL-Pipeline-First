89646c298d45a67a6e9e8564041f4c5818ad2abc
Njabulo Moshifa <moshifanjabulo@gmail.com>
2026-06-20 21:52:45 +0200

    Managed to do invalid records to be on a csv side

--- src/validate.py
+++ src/validate.py
@@ -2,6 +2,7 @@ import pandas as pd
 from src.extract import load_sales
 from config.config import GROK
 from groq import Groq
+from pathlib import Path
 
 # In vadilation we will report
 # Check for bads    
@@ -14,7 +15,7 @@ from groq import Groq
 
 # def report_invalid_records()
 
-def report_invalid(all_records: int, failed: int):
+def report(all_records: int, failed: int):
     with open(file="reports/report.txt", mode="w") as file:
         file.write(f"Records Read: {all_records}\n")
         file.write(f"Valid Records: {all_records-failed}\n")
@@ -37,22 +38,80 @@ def validate(fileData: pd.DataFrame):
                                                duplicates_,
                                                strange_prices_
     )
-    print(failed_data)
-    report_invalid(len(fileData), failed_data)
+    report(len(fileData), failed_data)
+
+    report_csv(prices_negatives, quantities_negatives, duplicates_, strange_prices_, unsupported_opperand_price, unsupported_opperand_quantites)
+    # print(empty_columns)
+    # print()
+    # print(f"Negatives Prices: \n{prices_negatives}")
+    # print()
+    # print(f"Quantities Negatives: \n{quantities_negatives}")
+    # print()
+    # print(f"Duplicates\n{duplicates_}")
+    # print()
+    # print(f"Strange Prices\n{strange_prices_}")
 
 
-    print(empty_columns)
-    print()
-    print(f"Negatives Prices: \n{prices_negatives}")
-    print()
-    print(f"Quantities Negatives: \n{quantities_negatives}")
-    print()
-    print(f"Duplicates\n{duplicates_}")
-    print()
-    print(f"Strange Prices\n{strange_prices_}")
+def report_csv(negative_prices=None, negative_quantitiy=None, dublicates_=None, strange=None, unsupported_opperand_price=None, unsupported_opperand_quantites=None):
+
+    # prices
+    if negative_prices != None:
+        for transaction_id in negative_prices:
+            write_to_csv(transaction_id, "Negative Price")
+
+    if negative_quantitiy != None:
+        for transaction_id in negative_quantitiy:
+            write_to_csv(transaction_id, "Negative quantities")
+
+    if dublicates_ != None:
+        for transaction_id in dublicates_:
+            write_to_csv(transaction_id[0], "Dublicates")
+
+    if strange != None:
+        for transaction_id in strange:
+            write_to_csv(transaction_id, "We have strange transactions like quantity is bigger than price")
+
+    
+    if unsupported_opperand_price != None:
+        for  transaction_id in unsupported_opperand_price:
+            write_to_csv(transaction_id, "Uknown types like we got alphabets or symbols instead of numbers from price")
+
+    
+    if unsupported_opperand_quantites != None:
+        for  transaction_id in unsupported_opperand_quantites:
+            write_to_csv(transaction_id, "Uknown types like we got alphabets or symbols instead of numbers from quantity")
+
+    
+    
+def write_to_csv(transaction_id: int, description: str):
+
+    data = {
+        "transaction_id" : [transaction_id],
+        "description": [description]
+    }
+
+    dir_path = Path("reports")
+
+    file_path = dir_path / "invalid_records.csv"
+    dir_path.mkdir(parents=True, exist_ok=True)
+    if not file_path.is_file() or file_path.stat().st_size == 0:
+
+        df = pd.DataFrame(data)
+        df.to_csv(file_path, index=False)
+
+    else:
+        df = pd.DataFrame(data)
+        original = pd.read_csv(file_path)
+
+
+        updated_df = pd.concat([original, df], ignore_index=True)
+        updated_df.to_csv(file_path, index=False)
+
+        
+    
+
 
 def calculate_everyFailed_method(*args) -> int:
-    print(args)
     return  sum([len(i) for i in args])
 
 def negative_quantites(file_data: pd.DataFrame) -> list[tuple[int]]:   
@@ -107,28 +166,13 @@ def missing_columns(file_data: pd.DataFrame) -> dict:
                 df = pd.DataFrame(file_data)
                 df.to_csv("data/sales.csv", index=False)
 
-
-
-
-        if pd.isna(row["quantity"]):
-            if "quantity" in missing_values.keys():
-                missing_values["quantity"].append([row["transaction_id"]])
-                continue
-            missing_values["quantity"] = [[row["transaction_id"]]]
-
-        if pd.isna(row["price"]):
-            if "price" in missing_values.keys():
-                missing_values["price"].append([row["transaction_id"]])
-                continue
-            missing_values["price"] = [[row["transaction_id"]]]
-
     return missing_values
 
 def duplicates(file_data: pd.DataFrame) -> list[tuple[int]]:
     all_transactions = [int(row["transaction_id"]) for index, row in file_data.iterrows()]
     counting = {num: all_transactions.count(num) for num in all_transactions}
     duplicates_ = [(row, counting[row]) for row in counting if counting[row] > 1]
-    return duplicates_
+    return duplicates_ 
 
 def get_product_category(product_name: str) -> str:
     """Classifies a product into one of the predefined dataset categories."""
